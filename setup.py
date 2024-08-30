a5bf6fd24bf69911ee397210e328a059878068bf
mombrei <axel.mombrei@dlr.rlp.de>
2024-08-30 11:52:24 +0200

    fixes

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='mo_toolbox',
-    version='0.1.1',
-    packages=['tools'],
-    install_requires=['shapely>=2.0.6', 'geopandas>=1.0.1', 'pandas>=2.2.2', 'ttkwidgets>=0.13.0'],
+    version='0.1.1.2',
+    packages=['mo_toolbox'],
+    install_requires=['shapely>=2.0.6', 'geopandas>=1.0.1', 'pandas>=2.2.2', 'ttkwidgets>=0.13.0', "openpyxl>=3.1.5"],
+    package_data={'mo_toolbox': ['*.json', '*xlsx', '*pkl']},
     url='http://10.195.64.254:81/mombrei/mo_toolbox.git',
     author='Axel Mombrei',
     author_email='axel.mombrei@dlr.rlp.de',
     description='Private Toolsammlung'
 )
-
